0f14e4ba50d7759c1f7a7cdd52b825bbe38fbd8d
hameeem <hameembaba20@gmail.com>
2026-09-08 11:28:51 +0530

    Configure GitHub Pages deployment, next.config.js export, and GitHub Actions workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: isProd ? '/MyPortfolio' : '',
+  assetPrefix: isProd ? '/MyPortfolio/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
